49808537003a10d8b44cf6d815a761ad685d8714
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-30 20:35:30 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pets search filter for WP, using SQL
+
+A simple plugin that let me learn how to access SQL directly. Following along a Udemy tutorial.
+Created by lilKriT.
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+    Plugin Name: Pets Filter SQL
+    Version: 1.0
+    Author: lilKriT
+    Author URI: https://lilkrit.dev
+*/
+
+if (!defined("ABSPATH")) exit;
+
+class PetFilter
+{
+    function __construct()
+    {
+    }
+}
+
+$petFilter = new PetFilter();
